c698d2bcaa804a7f501603103ef9d423aa5395d6
elpropio2024 <luisferpaezfuentes@gmail.com>
2025-07-28 16:18:53 -0500

    Add .next/static/chunks/src_app_simulator_page_tsx_a993527f._.js 1753737532378

---
+++ .next/static/chunks/src_app_simulator_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_4a312bec._.js",
+  "static/chunks/node_modules_601a69dc._.js"
+],
+    source: "dynamic"
+});
